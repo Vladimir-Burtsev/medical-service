b470e9df7b1dd6e66e027026bd744aef2c4941a0
Anton Poliakov <p-anton-v@yandex.ru>
2024-05-05 20:08:37 +0500

    Добавлена отправка сообщения пользователю об отмени записис к врачу и отправкии сообщения в аудит

--- src/main/java/academy/kata/mis/medicalservice/model/dto/feign/PersonDto.java
+++ src/main/java/academy/kata/mis/medicalservice/model/dto/feign/PersonDto.java
@@ -1,4 +1,7 @@
 package academy.kata.mis.medicalservice.model.dto.feign;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public record PersonDto(long id, String firstName, String lastName) {
 }
--- src/main/java/academy/kata/mis/medicalservice/service/TalonBusinessService.java
+++ src/main/java/academy/kata/mis/medicalservice/service/TalonBusinessService.java
@@ -6,5 +6,6 @@ import java.util.UUID;
 public interface TalonBusinessService {
     void cancelReservationTalon(Long talonId);
     boolean existsTalonByIdAndPatientUserId(Long talonId, UUID userId);
+    String getResponseTalonCancel(Long talonId);
     GetAssignedTalonsByPatientResponse getAllPatientTalonByPatientId(long patientId);
 }
