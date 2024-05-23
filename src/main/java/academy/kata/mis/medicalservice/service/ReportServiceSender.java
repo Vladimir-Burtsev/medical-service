71981b0247887a74a9fa09f4ea36d48794ac9c31
Anton Poliakov <p-anton-v@yandex.ru>
2024-05-24 01:55:17 +0500

    add sendInMessageService

--- src/main/java/academy/kata/mis/medicalservice/service/ReportServiceSender.java
+++ src/main/java/academy/kata/mis/medicalservice/service/ReportServiceSender.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 public interface ReportServiceSender {
     void sendInReportService(UUID userId, boolean email, Appeal appeal, UUID operationId);
     String generateInfo(Appeal appeal);
-    void sendInReportService(UUID userId, String email, String info, UUID operationId);
+
+//    void sendInReportService(UUID userId, String email, String info, UUID operationId);
     void sendInMessageService(String userEmail, String subject, String text);
 }
