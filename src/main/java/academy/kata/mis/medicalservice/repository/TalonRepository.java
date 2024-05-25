447dcdb59856c73012f67167130cc30aad8ce0f5
Anton Poliakov <email@mail.ru>
2024-05-25 17:40:03 +0500

    add EventMessage

--- src/main/java/academy/kata/mis/medicalservice/repository/TalonRepository.java
+++ src/main/java/academy/kata/mis/medicalservice/repository/TalonRepository.java
@@ -25,4 +25,6 @@ public interface TalonRepository extends JpaRepository<Talon, Long> {
         where t.patient.id = :patientId
         """)
     Set<Talon> findAllByPatientId(long patientId);
+    @Query("SELECT t.doctor.id FROM Talon t WHERE t.id=:talonId")
+    Long getDoctorIdByTalonId(@Param("talonId") Long talonId);
 }
