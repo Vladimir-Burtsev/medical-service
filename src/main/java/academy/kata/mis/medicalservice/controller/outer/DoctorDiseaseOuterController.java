cecf029f183478284025e8f67856ef44029742b4
Anton Poliakov <p-anton-v@yandex.ru>
2024-06-03 00:51:45 +0500

    add getDiseaseShortInfoPagination and getDiseaseDepShortInfoResponse

--- src/main/java/academy/kata/mis/medicalservice/controller/outer/DoctorDiseaseOuterController.java
+++ src/main/java/academy/kata/mis/medicalservice/controller/outer/DoctorDiseaseOuterController.java
@@ -9,7 +9,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
