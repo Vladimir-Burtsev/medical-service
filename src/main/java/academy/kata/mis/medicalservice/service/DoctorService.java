acb0a55e7b14e56d2a75f714aed65cdfb791e231
TripleArcan <nlogovchin@mail.ru>
2024-06-03 19:03:34 +0500

    fix

--- src/main/java/academy/kata/mis/medicalservice/service/DoctorService.java
+++ src/main/java/academy/kata/mis/medicalservice/service/DoctorService.java
@@ -5,7 +5,5 @@ import academy.kata.mis.medicalservice.model.entity.Doctor;
 import java.util.UUID;
 
 public interface DoctorService {
-    Doctor getDoctorIfExistsByUserIdAndId(UUID doctorUUID, long id);
-
     boolean isExistsByUserIdAndId(UUID doctorUUID, long id);
 }
