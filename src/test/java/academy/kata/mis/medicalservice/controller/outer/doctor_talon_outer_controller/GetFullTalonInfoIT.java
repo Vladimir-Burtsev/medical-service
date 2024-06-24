c2bb6f487661888ead702006c778f97d1375bdda
Luxa <iluxa3650@gmail.com>
2024-06-24 20:49:24 +0300

    add tests GetFullTalonInfoIT

---
+++ src/test/java/academy/kata/mis/medicalservice/controller/outer/doctor_talon_outer_controller/GetFullTalonInfoIT.java
@@ -0,0 +1,107 @@
+package academy.kata.mis.medicalservice.controller.outer.doctor_talon_outer_controller;
+
+import academy.kata.mis.medicalservice.ContextIT;
+import academy.kata.mis.medicalservice.model.dto.auth.JwtAuthentication;
+import academy.kata.mis.medicalservice.model.dto.auth.Role;
+import academy.kata.mis.medicalservice.util.JwtProvider;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Set;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+        value = "/scripts/controller/outer/doctor_talon_outer_controller/getFullTalonInfo.sql")
+@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+        value = "/scripts/clear.sql")
+public class GetFullTalonInfoIT extends ContextIT {
+
+    @MockBean
+    private JwtProvider jwtProvider;
+
+    private final String accessToken = "Bearer token";
+    private final String userId = "99efc3cd-c5e6-4469-ab40-ff97d3d98882";
+
+    @Test
+    public void getFullTalonInfo_success() throws Exception {
+        long talonId = 1L;
+        JwtAuthentication jwtInfoToken = new JwtAuthentication();
+        jwtInfoToken.setUserId(UUID.fromString(userId));
+        jwtInfoToken.setRoles(Set.of(new Role("DOCTOR")));
+        jwtInfoToken.setAuthenticated(true);
+        when(jwtProvider.getTokenFromRequest("Bearer token")).thenReturn("token");
+        when(jwtProvider.validateAccessToken("token")).thenReturn(true);
+        when(jwtProvider.getAuthentication("token")).thenReturn(jwtInfoToken);
+
+        mockMvc.perform(
+                get("/api/medical/doctor/talon/full/info")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", accessToken)
+                        .param("talon_id", String.valueOf(talonId))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.talonId", Is.is(1)))
+                .andExpect(jsonPath("$.visitTime", Is.is("2024-06-26")))
+                .andExpect(jsonPath("$.organization").doesNotExist())
+                .andExpect(jsonPath("$.organization").doesNotExist())
+                .andExpect(jsonPath("$.department.departmentId", Is.is(1)))
+                .andExpect(jsonPath("$.department.departmentName").doesNotExist())
+                .andExpect(jsonPath("$.cabinetNumber").doesNotExist())
+                .andExpect(jsonPath("$.doctor.doctorId", Is.is(1)))
+                .andExpect(jsonPath("$.doctor.doctorFirstName").doesNotExist())
+                .andExpect(jsonPath("$.doctor.doctorLastName").doesNotExist())
+                .andExpect(jsonPath("$.doctor.patronymic").doesNotExist())
+                .andExpect(jsonPath("$.doctor.doctorPositionName").doesNotExist())
+                .andExpect(jsonPath("$.patient.patientId", Is.is(1)))
+                .andReturn();
+    }
+
+    @Test
+    public void getFullTalonInfo_talonNotFound() throws Exception {
+        long talonId = 4L;
+        JwtAuthentication jwtInfoToken = new JwtAuthentication();
+        jwtInfoToken.setUserId(UUID.fromString(userId));
+        jwtInfoToken.setRoles(Set.of(new Role("DOCTOR")));
+        jwtInfoToken.setAuthenticated(true);
+        when(jwtProvider.getTokenFromRequest("Bearer token")).thenReturn("token");
+        when(jwtProvider.validateAccessToken("token")).thenReturn(true);
+        when(jwtProvider.getAuthentication("token")).thenReturn(jwtInfoToken);
+
+        mockMvc.perform(
+                        get("/api/medical/doctor/talon/full/info")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", accessToken)
+                                .param("talon_id", String.valueOf(talonId))
+                )
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(content().string("Талон не найден!"));
+    }
+
+    @Test
+    public void getFullTalonInfo_talonNotAssignedToAuthorizedDoctor() throws Exception {
+        long talonId = 3L;
+        JwtAuthentication jwtInfoToken = new JwtAuthentication();
+        jwtInfoToken.setUserId(UUID.fromString(userId));
+        jwtInfoToken.setRoles(Set.of(new Role("DOCTOR")));
+        jwtInfoToken.setAuthenticated(true);
+        when(jwtProvider.getTokenFromRequest("Bearer token")).thenReturn("token");
+        when(jwtProvider.validateAccessToken("token")).thenReturn(true);
+        when(jwtProvider.getAuthentication("token")).thenReturn(jwtInfoToken);
+
+        mockMvc.perform(
+                        get("/api/medical/doctor/talon/full/info")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", accessToken)
+                                .param("talon_id", String.valueOf(talonId))
+                )
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(content().string("Авторизованный пользователь не является доктором, которому принадлежит талон"));
+    }
+}
---
+++ src/test/resources/scripts/controller/outer/doctor_talon_outer_controller/getFullTalonInfo.sql
@@ -0,0 +1,24 @@
+insert into test.organizations(id)
+values (1);
+
+insert into test.departments(id, organization_id)
+values (1, 1),
+       (2, 1);
+
+insert into test.patients(id, person_id, user_id, organization_id)
+values (1, 1, '83bb225a-ad24-474f-a6e8-64a26c7db63f', 1),
+       (2, 2, '1ce6590e-9dcc-4a58-afea-b1fb58d9342b', 1);
+
+insert into test.doctors(id, person_id, user_id, position_id, department_id)
+values (1, 1, '99efc3cd-c5e6-4469-ab40-ff97d3d98882', 1, 1),
+       (2, 2, 'cc70ff03-5637-46da-9dc4-91ccbd8c51c3', 1, 2);
+
+insert into test.talons(id, time, patient_id, doctor_id)
+values (1, '2024-06-26 13:00:00.000000', 1, 1),
+       (2, '2024-06-26 14:00:00.000000', 2, 1),
+       (3, '2024-06-26 15:00:00.000000', null, 2);
+
+
+
+
+
