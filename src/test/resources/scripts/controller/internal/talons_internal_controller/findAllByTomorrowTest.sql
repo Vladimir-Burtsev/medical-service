2116b33b20974c913d293e195b7b0664ec3d496d
Степанова_Елизавета <stepanowa.liza2001@gmail.com>
2024-06-27 15:02:28 +0300

    MIS-30

--- src/test/resources/scripts/controller/internal/talons_internal_controller/findAllByTomorrowTest.sql
+++ src/test/resources/scripts/controller/internal/talons_internal_controller/findAllByTomorrowTest.sql
@@ -12,9 +12,9 @@ values (1, 1, '662b6f6e-4702-44c4-98f4-e73243087d46', 1),
        (2, 2, '599d9ef0-7ae0-4924-890b-55eb13f85e53', 1);
 
 insert into test.talons(id, time, patient_id, doctor_id)
-values (1, '2024-06-23 10:00:00.000000', 1, 1),
-       (2, '2024-05-17 10:00:00.000000', 1, 1),
-       (3, '2024-06-23 10:00:00.000000', null, 1);
+values (1, now() + interval '1 day', 1, 1),
+       (2, now() + interval '1 day', null, 1),
+       (3, now() + interval '1 day', null, 1);
 
 
 
