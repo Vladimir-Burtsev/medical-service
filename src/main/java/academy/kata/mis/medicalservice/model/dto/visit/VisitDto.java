f7d4830173c60920603999fedc5f05f46968f751
vladimir burtsev <amakusl2@gmail.com>
2024-07-16 21:45:10 +0300

    add builder annotation

--- src/main/java/academy/kata/mis/medicalservice/model/dto/visit/VisitDto.java
+++ src/main/java/academy/kata/mis/medicalservice/model/dto/visit/VisitDto.java
@@ -2,10 +2,12 @@ package academy.kata.mis.medicalservice.model.dto.visit;
 
 import academy.kata.mis.medicalservice.model.dto.doctor.DoctorShortDto;
 import academy.kata.mis.medicalservice.model.dto.service.MedicalServiceShortDto;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
 public record VisitDto(long visitId,
                        LocalDateTime visitTime,
                        DoctorShortDto doctor,
