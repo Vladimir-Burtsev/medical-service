60dd12a1a3b26598dbb9983bb344d1d031edb24b
vladimir burtsev <amakusl2@gmail.com>
2024-07-20 20:19:39 +0300

    Add integration tests for visit info controller
    - Implemented test for successful retrieval of visit information (getVisitInfoIT_success).
    - Added test for department mismatch scenario (getVisitInfoIT_departmentsDoNotMatch).
    - Updated test data and validation to align with code changes.

--- src/test/java/academy/kata/mis/medicalservice/controller/outer/doctor_visit_rest_controller/GetVisitInfoTest.java
+++ src/test/java/academy/kata/mis/medicalservice/controller/outer/doctor_visit_rest_controller/GetVisitInfoTest.java
@@ -15,8 +15,7 @@ import java.util.UUID;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, value =
         "/scripts/controller/outer/doctor_visit_rest_controller/getVisitInfo.sql")
@@ -28,36 +27,51 @@ public class GetVisitInfoTest extends ContextIT {
     private final String accessToken = "Bearer token";
     private final Set<Role> roles =
             Set.of(new Role("DOCTOR"), new Role("CHIEF_DOCTOR"), new Role("DIRECTOR"));
-    @Test
-    public void getVisitInfo_success() throws Exception {
-        String user = "123e4567-e89b-12d3-a456-426614174000";
-//проверка токена
+    private final String user = "123e4567-e89b-12d3-a456-426614174000";
+    private void setupJwtTokenMock (String userId) throws Exception {
         JwtAuthentication jwtInfoToken = new JwtAuthentication();
-        jwtInfoToken.setUserId(UUID.fromString(user));
+        jwtInfoToken.setUserId(UUID.fromString(userId));
         jwtInfoToken.setRoles(roles);
         jwtInfoToken.setAuthenticated(true);
 
         when(jwtProvider.getTokenFromRequest("Bearer token")).thenReturn("token");
         when(jwtProvider.validateAccessToken("token")).thenReturn(true);
         when(jwtProvider.getAuthentication("token")).thenReturn(jwtInfoToken);
+    }
+    @Test
+    public void getVisitInfoIT_success() throws Exception {
+
+        setupJwtTokenMock(user);
 
         mockMvc.perform(
                         get("/api/medical/doctor/visit/info")
-                                .param("visit_id", "1")
+                                .param("visit_id", "1001")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .header("Authorization", accessToken)
                 )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.visitId", Is.is(1)))
+                .andExpect(jsonPath("$.visitId", Is.is(1001)))
                 .andExpect(jsonPath("$.visitTime", Is.is("2023-07-01T12:00:00")))
-                .andExpect(jsonPath("$.doctor.doctorId", Is.is(1)))
-                .andExpect(jsonPath("$.doctor.doctorFirstName", Is.is("DoctorFirstName")))
-                .andExpect(jsonPath("$.doctor.doctorLastName", Is.is("DoctorLastName")))
-                .andExpect(jsonPath("$.doctor.patronymic", Is.is("Patronymic")))
-                .andExpect(jsonPath("$.doctor.doctorPositionName", Is.is("DoctorPositionName")))
-                .andExpect(jsonPath("$.medicalServices[0].medicalServiceDepId", Is.is(1)))
+                .andExpect(jsonPath("$.doctor.doctorId", Is.is(1001)))
+                .andExpect(jsonPath("$.doctor.doctorFirstName", Is.is("FAKEDoctorFirstName")))
+                .andExpect(jsonPath("$.doctor.doctorLastName", Is.is("FAKEDoctorLastName")))
+                .andExpect(jsonPath("$.doctor.patronymic", Is.is("FAKEPatronymic")))
+                .andExpect(jsonPath("$.doctor.doctorPositionName", Is.is("FAKEDoctorPositionName")))
+                .andExpect(jsonPath("$.medicalServices[0].medicalServiceDepId", Is.is(1001)))
                 .andExpect(jsonPath("$.medicalServices[0].serviceIdentifier", Is.is("ServiceIdentifier")))
                 .andExpect(jsonPath("$.medicalServices[0].serviceName", Is.is("ServiceName")));
+    }
 
+    @Test
+    public void getVisitInfoIT_departmentsDoNotMatch () throws Exception {
+        setupJwtTokenMock(user);
+
+        mockMvc.perform(
+                get("/api/medical/doctor/visit/info")
+                .param("visit_id", "1004")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", accessToken)
+        )
+                .andExpect(status().isForbidden());
     }
 }
