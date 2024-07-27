b9cb9459fb9ee089a71ca8392e8e2aeabedb290e
vladimir burtsev <amakusl2@gmail.com>
2024-07-27 22:49:41 +0300

    Фича: добавить взаимодействие с зависимыми сервисами - замокал обращение в Person и Structure сервисы - симулировал ответы - верифицировал вызовы моков

--- src/test/java/academy/kata/mis/medicalservice/controller/outer/doctor_visit_rest_controller/GetVisitInfoTest.java
+++ src/test/java/academy/kata/mis/medicalservice/controller/outer/doctor_visit_rest_controller/GetVisitInfoTest.java
@@ -1,8 +1,12 @@
 package academy.kata.mis.medicalservice.controller.outer.doctor_visit_rest_controller;
 
 import academy.kata.mis.medicalservice.ContextIT;
+import academy.kata.mis.medicalservice.feign.PersonFeignClient;
+import academy.kata.mis.medicalservice.feign.StructureFeignClient;
 import academy.kata.mis.medicalservice.model.dto.auth.JwtAuthentication;
 import academy.kata.mis.medicalservice.model.dto.auth.Role;
+import academy.kata.mis.medicalservice.model.dto.department_organization_position_cabinet.DepartmentOrganizationPositionCabinetNameDto;
+import academy.kata.mis.medicalservice.model.dto.doctor.DoctorShortDto;
 import academy.kata.mis.medicalservice.util.JwtProvider;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
@@ -13,7 +17,7 @@ import org.springframework.test.context.jdbc.Sql;
 import java.util.Set;
 import java.util.UUID;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,10 +28,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class GetVisitInfoTest extends ContextIT {
     @MockBean
     private JwtProvider jwtProvider;
+    @MockBean
+    private PersonFeignClient personFeignClient;
+    @MockBean
+    private StructureFeignClient structureFeignClient;
     private final String accessToken = "Bearer token";
     private final Set<Role> roles =
             Set.of(new Role("DOCTOR"), new Role("CHIEF_DOCTOR"), new Role("DIRECTOR"));
     private final String user = "123e4567-e89b-12d3-a456-426614174000";
+
     private void setupJwtTokenMock (String userId) throws Exception {
         JwtAuthentication jwtInfoToken = new JwtAuthentication();
         jwtInfoToken.setUserId(UUID.fromString(userId));
@@ -38,33 +47,58 @@ public class GetVisitInfoTest extends ContextIT {
         when(jwtProvider.validateAccessToken("token")).thenReturn(true);
         when(jwtProvider.getAuthentication("token")).thenReturn(jwtInfoToken);
     }
+    private void feignClientMocks () {
+        when(structureFeignClient.getDepartmentOrganizationPositionCabinetNameDto(3001))
+                .thenReturn(new DepartmentOrganizationPositionCabinetNameDto(
+                        1001,
+                        "terapist",
+                        1001,
+                        "organization name",
+                        "position name",
+                        "cabinet num"
+                ));
+        when(personFeignClient.getDoctorShortDtoByPersonIdAndDoctorId(2001, 1001))
+                .thenReturn(new DoctorShortDto(
+                        1001,
+                        "DoctorFirstName",
+                        "DoctorLastName",
+                        "DoctorPatronymic",
+                        "position name"
+                ));
+    }
     @Test
     public void getVisitInfoIT_success() throws Exception {
-
+        long visitId = 1001L;
         setupJwtTokenMock(user);
+        feignClientMocks();
 
         mockMvc.perform(
                         get("/api/medical/doctor/visit/info")
-                                .param("visit_id", "1001")
+                                .param("visit_id", String.valueOf(visitId))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .header("Authorization", accessToken)
                 )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.visitId", Is.is(1001)))
+                .andExpect(jsonPath("$.visitId", Is.is((int) visitId)))
                 .andExpect(jsonPath("$.visitTime", Is.is("2023-07-01T12:00:00")))
                 .andExpect(jsonPath("$.doctor.doctorId", Is.is(1001)))
-                .andExpect(jsonPath("$.doctor.doctorFirstName", Is.is("FAKEDoctorFirstName")))
-                .andExpect(jsonPath("$.doctor.doctorLastName", Is.is("FAKEDoctorLastName")))
-                .andExpect(jsonPath("$.doctor.patronymic", Is.is("FAKEPatronymic")))
-                .andExpect(jsonPath("$.doctor.doctorPositionName", Is.is("FAKEDoctorPositionName")))
+                .andExpect(jsonPath("$.doctor.doctorFirstName", Is.is("DoctorFirstName")))
+                .andExpect(jsonPath("$.doctor.doctorLastName", Is.is("DoctorLastName")))
+                .andExpect(jsonPath("$.doctor.patronymic", Is.is("DoctorPatronymic")))
+                .andExpect(jsonPath("$.doctor.doctorPositionName", Is.is("position name")))
                 .andExpect(jsonPath("$.medicalServices[0].medicalServiceDepId", Is.is(1001)))
                 .andExpect(jsonPath("$.medicalServices[0].serviceIdentifier", Is.is("ServiceIdentifier")))
                 .andExpect(jsonPath("$.medicalServices[0].serviceName", Is.is("ServiceName")));
+        verify(structureFeignClient, times(1))
+                .getDepartmentOrganizationPositionCabinetNameDto(3001);
+        verify(personFeignClient, times(1))
+                .getDoctorShortDtoByPersonIdAndDoctorId(2001, 1001);
     }
 
     @Test
     public void getVisitInfoIT_VisitNotFound() throws Exception {
         setupJwtTokenMock(user);
+        feignClientMocks();
 
         mockMvc.perform(
                 get("/api/medical/doctor/visit/info")
@@ -73,10 +107,16 @@ public class GetVisitInfoTest extends ContextIT {
                 .header("Authorization", accessToken)
         )
                 .andExpect(status().isNotFound());
+        verify(structureFeignClient, times(0))
+                .getDepartmentOrganizationPositionCabinetNameDto(3001);
+        verify(personFeignClient, times(0))
+                .getDoctorShortDtoByPersonIdAndDoctorId(2001, 1001);
     }
+
     @Test
     public void getVisitInfoIT_departmentsDoNotMatch () throws Exception {
         setupJwtTokenMock(user);
+        feignClientMocks();
 
         mockMvc.perform(
                 get("/api/medical/doctor/visit/info")
@@ -85,5 +125,9 @@ public class GetVisitInfoTest extends ContextIT {
                 .header("Authorization", accessToken)
         )
                 .andExpect(status().isForbidden());
+        verify(structureFeignClient, times(1))
+                .getDepartmentOrganizationPositionCabinetNameDto(3002);
+        verify(personFeignClient, times(1))
+                .getDoctorShortDtoByPersonIdAndDoctorId(2002, 1002);
     }
 }
